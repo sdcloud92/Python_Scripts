255c0f7c014ee1c97a45c62b199bb265e2d0daa9
Tait Hoglund <hogtai@gmail.com>
2023-02-03 03:33:43 +0000

    Added documentation for python script

--- Week12MiniProjectOptional.py
+++ Week12MiniProjectOptional.py
@@ -1,5 +1,9 @@
+# imports needed dependencies for script
+
 import random
 import string
+
+# function name, uses a loop to retrieve random alpha-string characters, attaches to assigned department, returns results to screen
 
 def generate_names(num_names, department):
     names = []
@@ -7,6 +11,8 @@ def generate_names(num_names, department):
         name = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=8))
         names.append(f"{name} ({department})")
     return names
+
+# Assigns variables from user inputs, non-case-sensitive, returns prompt for invalid inputs, prints results to screen
 
 num_names = int(input("How many instance names do you require? "))
 department = input("Assign a department (Marketing, Accounting, or FinOps): ").lower()
