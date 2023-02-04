3fd498630a861417465ea0680b4abbf35dcb2c84
Tait Hoglund <hogtai@gmail.com>
2023-02-04 15:11:36 +0000

    Conversions STR to INT

---
+++ ConverstionSTRtoINT.py
@@ -0,0 +1,22 @@
+# prints a rounded value to the screen of number
+
+print(round(2.9))
+
+# Assigns a variable x from the input of a user
+
+x = input("x: ")
+
+#assigns a variable 'y', converts variable x to an int, adds 1
+
+y = int(x) +1
+
+# prints the value of variables x & y to the screen
+
+print(f"x: {x}, y: {y}")
+
+# int(x) - converts contents of variable x to a whole number
+# float(x) - converts contents of variable x to a floating number
+# str(x) - Converts contents of variable x to a string
+# bool(x) - converts contents of variable x to either "truthy" or "falsy"
+# Values that are considered "falsy" - empty strings "" , "0", "None" in boolean
+# all other values in boolean will be considered "truthy"
