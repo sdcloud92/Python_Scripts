2de9e69372d88dcb4ea0b881e441f4a949210f81
Tait Hoglund <hogtai@gmail.com>
2023-02-04 02:58:22 +0000

    clarified names of variables "number_of_names_needed"

--- Week12MiniProjectOptional.py
+++ Week12MiniProjectOptional.py
@@ -5,20 +5,20 @@ import string
 
 # function name, uses a loop to retrieve random alpha-string characters, attaches to assigned department, returns results to screen
 
-def generate_names(num_names, department):
+def generate_names(number_of_names_needed, department):
     names = []
-    for i in range(num_names):
+    for i in range(number_of_names_needed):
         name = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=8))
         names.append(f"{name} ({department})")
     return names
 
 # Assigns variables from user inputs, non-case-sensitive, returns prompt for invalid inputs, prints results to screen
 
-num_names = int(input("How many instance names do you require? "))
+number_of_names_needed = int(input("How many instance names do you require? "))
 department = input("Assign a department (Marketing, Accounting, or FinOps): ").lower()
 
 if department not in ["marketing", "accounting", "finops"]:
     print("You can only specify the following departments: Marketing, Accounting, or FinOps. Please try again and only specify a valid department.")
 else:
-    names = generate_names(num_names, department.capitalize())
+    names = generate_names(number_of_names_needed, department.capitalize())
     print(names)
