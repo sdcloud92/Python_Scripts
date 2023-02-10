e46afe642cb3a59f5831c0896379133192dbe6fc
Tait Hoglund <hogtai@gmail.com>
2023-02-10 02:24:53 +0000

    daily work

---
+++ recursive.py
@@ -0,0 +1,16 @@
+
+
+def summation(x):
+   if(x ==1):
+       return 1
+    else:
+        return x + summation(x-1)
+   
+   print(1+2)
+   print(1 + 2 + 3)
+   print(1 + 2 + 3 +4)
+   
+   print(summation(2))
+   print(summation(3))
+   print(summation(4))
+  
